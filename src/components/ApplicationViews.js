30493a35501c4ae92b782f5e3448f86d2b3692f0
Kirren Covey <kirrencovey@gmail.com>
2019-02-27 11:57:56 -0600

    moved data to api, get status using componentDidMount

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -7,43 +7,29 @@ import StoreList from './store/StoreList'
 
 class ApplicationViews extends Component {
 
-    storeArray = [
-        { id: 1, name: "Kandy Korner", address: "123 Sugar Street" },
-        { id: 2, name: "Kandy Kraze", address: "808 Licorice Drive" }
-    ]
-
-    employeeArray = [
-        { id: 1, name: "Mo", position: "HBIC of Sugar"},
-        { id: 2, name: "Nathan", position: "Lead Candy Sampler" },
-        { id: 3, name: "Warner", position: "Web Design & Marketing" },
-        { id: 4, name: "Kirren", position: "Nap Coordinater" }
-    ]
-
-    candyTypeArray = [
-        { id: 1, name: "Hard" },
-        { id: 2, name: "Gummy" },
-        { id: 3, name: "Bar" },
-        { id: 4, name: "Chocolate" },
-        { id: 5, name: "Other" }
-    ]
-
-    candyArray = [
-        { id: 1, name: "Dove Promise", candyTypeId: 4 },
-        { id: 2, name: "Jolly Rancher", candyTypeId: 1 },
-        { id: 3, name: "Laffy Taffy", candyTypeId: 5 },
-        { id: 4, name: "Sour Worms", candyTypeId: 2 },
-        { id: 5, name: "Snickers", candyTypeId: 3 },
-        { id: 6, name: "Reeses", candyTypeId: 4 },
-        { id: 7, name: "Milky Way", candyTypeId: 3 },
-        { id: 8, name: "Gummy Bears", candyTypeId: 2 },
-        { id: 9, name: "LifeSavers", candyTypeId: 1 }
-    ]
-
     state = {
-        stores: this.storeArray,
-        employees: this.employeeArray,
-        candyTypes: this.candyTypeArray,
-        candies: this.candyArray
+        stores: [],
+        employees: [],
+        candyTypes: [],
+        candies: []
+    }
+
+    componentDidMount() {
+        const newState = {}
+
+        fetch("http://localhost:5002/candies")
+            .then(r => r.json())
+            .then(candies => newState.candies = candies)
+            .then(() => fetch("http://localhost:5002/employees")
+            .then(r => r.json()))
+            .then(employees => newState.employees = employees)
+            .then(() => fetch("http://localhost:5002/stores")
+            .then(r => r.json()))
+            .then(stores => newState.stores = stores)
+            .then(() => fetch("http://localhost:5002/candyTypes")
+            .then(r => r.json()))
+            .then(candyTypes => newState.candyTypes = candyTypes)
+            .then(() => this.setState(newState))
     }
 
     render() {
